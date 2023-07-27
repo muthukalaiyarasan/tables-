8a5ff5c19c77b1f74e67cef33bf59904eb6ac743
muthukalaiyarasan <132567401+muthukalaiyarasan@users.noreply.github.com>
2023-07-27 11:20:36 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// let x=parseInt(prompt("enter the number"));
+// let result;
+  // for(i=1;i<=x;i++){
+	  // result=i*x
+	  
+  // }
+       // document.write(i + "*" + x "=" result);
+	   
+	   
+	   
+	     let a=parseInt(prompt("Enter the number"))
+                                  for(i=1;i<=10;i++){
+                                          let b=i*a
+                                  document.write(i,"*",a,"=",b)
+                              console.log(i,"*",a,"=",b)
+                                 }
